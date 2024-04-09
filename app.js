5821b230252e6b59a99019e1e4fa3707dde9e0e8
celiajrl <celiajimenezreal@gmail.com>
2024-04-09 14:36:24 +0200

    conn ipw

--- app.js
+++ app.js
@@ -18,13 +18,14 @@ const upload = multer({ storage: storage })
 const app = express();
 app.use(express.json());
 app.use(cors());
-const PORT = process.env.PORT || 3000; 
 // db connection
 let db;
+const PORT = process.env.PORT || 3000; 
+
 
 connectToDb((err) => {  
     if (!err) {
-        app.listen(PORT, "0.0.0.0", () => {
+        app.listen(PORT, "0.0.0.0", function (){
             console.log(`Servidor escuchando en el puerto ${PORT}`);
         });
         db = getDb();
